55e639a9d3086b66cd175415ce71c1567f478732
anloffredo <loffredo.ap@gmail.com>
2019-10-23 00:59:40 -0700

    Framework extraction update
    Framework extraction update

---
+++ EmailMessage.cs
@@ -0,0 +1,115 @@
+﻿using Penguin.Cms.Abstractions.Attributes;
+using Penguin.Cms.Email.Abstractions;
+using Penguin.Cms.Email.Abstractions.Extensions;
+using Penguin.Cms.Email.Abstractions.Interfaces;
+using Penguin.Cms.Entities;
+using Penguin.Cms.Files;
+using Penguin.Email.Abstractions.Interfaces;
+using Penguin.Files.Abstractions;
+using Penguin.Persistence.Abstractions.Attributes.Control;
+using Penguin.Persistence.Abstractions.Attributes.Rendering;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Penguin.Cms.Email
+{
+    /// <summary>
+    /// Provides a database persistable representation of an email object
+    /// </summary>
+    public class EmailMessage : Entity, IPersistableEmailMessage
+    {
+        /// <summary>
+        /// A list of Database Files attached to this message
+        /// </summary>
+        [EagerLoad(1)]
+        [DontAllow(DisplayContexts.Any)]
+        public List<DatabaseFile> Attachments { get; } = new List<DatabaseFile>();
+
+        IEnumerable<IFile> IEmailMessage.Attachments => Attachments;
+
+        IEnumerable<string> IEmailMessage.BCCRecipients => this.GetBCCRecipients();
+
+        /// <summary>
+        /// A ; delimited list of BCC recipients
+        /// </summary>
+        [Display(Order = -600)]
+        public string BCCRecipients { get; set; }
+
+        /// <summary>
+        /// The HTML body of the email message
+        /// </summary>
+        [Display(Order = -400)]
+        [DisplayType("System.String.Html")]
+        [DontAllow(DisplayContexts.List)]
+        public virtual string Body { get; set; }
+
+        IEnumerable<string> IEmailMessage.CCRecipients => this.GetCCRecipients();
+
+        /// <summary>
+        /// A ; delimited list of CC recipients
+        /// </summary>
+        [Display(Order = -700)]
+        public string CCRecipients { get; set; }
+
+        /// <summary>
+        /// A string representing the account the email should be sent from, used when gathering connection information. Blank sends from default
+        /// </summary>
+        [Display(Order = -900)]
+        [DontAllow(DisplayContexts.List)]
+        public string From { get; set; }
+
+        /// <summary>
+        /// All messages are HTML by default. Set to false to override
+        /// </summary>
+        public bool IsHtml { get; set; } = true;
+
+        /// <summary>
+        /// An internal label for associating emails with the process that created them
+        /// </summary>
+        public string Label { get; set; }
+
+        IEnumerable<string> IEmailMessage.Recipients => this.GetRecipients();
+
+        /// <summary>
+        /// A ; delimited list of Message recipients
+        /// </summary>
+        [Display(Order = -800)]
+        public string Recipients { get; set; }
+
+        /// <summary>
+        /// The date the email message should be sent on. Defaults to the current time
+        /// </summary>
+        [DontAllow(DisplayContexts.Edit)]
+        public DateTime SendDate { get; set; } = DateTime.Now;
+
+        /// <summary>
+        /// The sent state of the message as persisted
+        /// </summary>
+        public EmailMessageState State { get; set; }
+
+        /// <summary>
+        /// The subject to send the message with
+        /// </summary>
+        [Display(Order = -500)]
+        public string Subject { get; set; }
+
+        public EmailMessage()
+        {
+        }
+
+        public EmailMessage(IEmailMessage message)
+        {
+            this.Attachments = message.Attachments.OfType<DatabaseFile>().ToList();
+
+            this.AddBCCRecipients(message.BCCRecipients.ToArray());
+            this.AddCCRecipients(message.CCRecipients.ToArray());
+            this.AddRecipients(message.Recipients.ToArray());
+
+            this.Body = message.Body;
+            this.From = message.From;
+            this.IsHtml = message.IsHtml;
+            this.Subject = message.Subject;
+        }
+    }
+}
---
+++ Services/EmailService.cs
@@ -0,0 +1,94 @@
+﻿using Penguin.Cms.Email.Abstractions;
+using Penguin.Configuration.Abstractions.Interfaces;
+using Penguin.DependencyInjection.Abstractions.Attributes;
+using Penguin.DependencyInjection.Abstractions.Enums;
+using Penguin.Email.Abstractions.Interfaces;
+using Penguin.Email.Services;
+using Penguin.Persistence.Abstractions.Interfaces;
+
+namespace Penguin.Cms.Email.Services
+{
+    /// <summary>
+    /// A service used to send/queue/persist new email messages
+    /// </summary>
+    [Register(ServiceLifetime.Scoped, typeof(ISendMail), typeof(IQueueMail), typeof(IQueueAndSendMail))]
+    public class EmailService : MailService, IQueueAndSendMail
+    {
+        /// <summary>
+        /// The IRepository implementation to be used when persisting the message
+        /// </summary>
+        protected IRepository<EmailMessage> EmailRepository { get; set; }
+
+        /// <summary>
+        /// Constructs a new instance of this service
+        /// </summary>
+        /// <param name="emailRepository">The IRepository implementation to be used when persisting the message</param>
+        /// <param name="configurationProvider">A Configuration provider to use to retrieve email message configurations</param>
+        /// <param name="messageBus">An optonal message bus to be used for publishing email related messages</param>
+        public EmailService(IRepository<EmailMessage> emailRepository, IProvideConfigurations configurationProvider) : base(configurationProvider)
+        {
+            EmailRepository = emailRepository;
+        }
+
+        /// <summary>
+        /// Queues a new email message by adding it to the IRepository implementation with the intent of being sent later by a worker
+        /// </summary>
+        /// <param name="message">The message to queue</param>
+        public void Queue(EmailMessage message) => EmailRepository.AddOrUpdate(message);
+
+        void IQueueMail.Queue(IEmailMessage message) => Queue(new EmailMessage(message));
+
+        /// <summary>
+        /// Persists the email message to the IRepository implementation and attempts to send it immediately bypassing any queue
+        /// </summary>
+        /// <param name="message">The message to send/persist</param>
+        public void QueueAndSend(EmailMessage message)
+        {
+            EmailRepository.AddOrUpdate(message);
+
+            Send(message);
+        }
+
+        void IQueueAndSendMail.QueueAndSend(IEmailMessage message) => QueueAndSend(new EmailMessage(message));
+
+        /// <summary>
+        /// Calls Queue as the default
+        /// </summary>
+        /// <param name="message">Queues the message</param>
+        public void QueueOrSend(EmailMessage message) => Queue(message);
+
+        void IQueueAndSendMail.QueueOrSend(IEmailMessage message) => QueueOrSend(new EmailMessage(message));
+
+        /// <summary>
+        /// Copies the provided message and requeues it with an optional new recipient
+        /// </summary>
+        /// <param name="message">The message to copy</param>
+        /// <param name="newRecipient">If not null, the value will replace the former recipient</param>
+        public void ReQueue(EmailMessage message, string newRecipient = null) => Queue(CopyMessage(message, newRecipient));
+
+        void IQueueMail.ReQueue(IEmailMessage message, string newRecipient = null) => ReQueue(new EmailMessage(message), newRecipient);
+
+        /// <summary>
+        /// Copies the provided message and requeues it with an optional new recipient, marks it sent, and then immediately sends it
+        /// </summary>
+        /// <param name="message">The message to copy</param>
+        /// <param name="newRecipient">If not null, the value will replace the former recipient</param>
+        public void ReQueueAndSend(EmailMessage message, string newRecipient = null) => QueueAndSend(CopyMessage(message, newRecipient, EmailMessageState.Success));
+
+        void IQueueAndSendMail.ReQueueAndSend(IEmailMessage message, string newRecipient = null) => ReQueueAndSend(new EmailMessage(message), newRecipient);
+
+        private EmailMessage CopyMessage(EmailMessage message, string newRecipient = null, EmailMessageState state = EmailMessageState.Unsent)
+        {
+            EmailMessage toReturn = EmailRepository.ShallowClone(message);
+
+            toReturn.State = state;
+
+            if (newRecipient != null)
+            {
+                toReturn.Recipients = newRecipient;
+            }
+
+            return toReturn;
+        }
+    }
+}
